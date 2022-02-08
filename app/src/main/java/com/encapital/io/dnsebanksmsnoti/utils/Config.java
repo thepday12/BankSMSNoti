2587109bc0a704b61efd91f5a1f670be19d03f66
Thep To Kim <kimthepit@gmail.com>
2022-02-08 14:28:10 +0700

    change app error log dir

--- app/src/main/java/com/encapital/io/dnsebanksmsnoti/utils/Config.java
+++ app/src/main/java/com/encapital/io/dnsebanksmsnoti/utils/Config.java
@@ -3,7 +3,7 @@ package com.encapital.io.dnsebanksmsnoti.utils;
 import android.os.Environment;
 
 public class Config {
-    public static final String CRASH_DIR= Environment.getExternalStorageDirectory() + "/bankSMS_crash/";
-    public static final String EXCEPTION_DIR= Environment.getExternalStorageDirectory() + "/bankSMS_exception/";
+    public static final String CRASH_DIR= Environment.getExternalStorageDirectory() + "/bankNotiSMS_crash/";
+    public static final String EXCEPTION_DIR= Environment.getExternalStorageDirectory() + "/bankNotiSMS_exception/";
 
 }
